c34247869563fbb2a3434fa87a0965fcd07306cc
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-08-05 13:29:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,9 @@ import css from './App.module.css';
 export const App = () => {
   return (
     <div className={css.appwrap}>
-      <h1 className={css.phohebookTitle}> Phonebook</h1>
+      <h1> Phonebook</h1>
       <FormContact />
-      <h2 className={css.contactsTitle}>Contacts</h2>
+      <h2>Contacts</h2>
       <Filter />
       <ListContacts />
     </div>
